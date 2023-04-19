acd6e46976db52a14d74eb268488ecb87ef40f75
gtambovtsev <gtambovtsev@inpolus.ru>
2023-04-19 16:13:27 +0300

    add data

--- telegram-bot/src/main/resources/db/changelog/changelog-insert_data.sql
+++ telegram-bot/src/main/resources/db/changelog/changelog-insert_data.sql
@@ -9,4 +9,35 @@ insert into sub_chapter(id, name, chapter_id) values ('458ed763-928c-4155-bee9-f
 
 insert into sub_chapter(id, name, chapter_id) values ('658ed763-928c-4155-bee9-fdbaaadc15f3', 'Used to', '128ed763-928c-4155-bee9-fdbaaadc15f3');
 
-insert into task(id, name, test, answers, sub_chapter_id) values ('758ed763-928c-4155-bee9-fdbaaadc15f3', 'First', 'He ... 22 years old', 'is', '258ed763-928c-4155-bee9-fdbaaadc15f3');
+INSERT INTO public.task (id, name, test, answers, sub_chapter_id) VALUES ('c5dab062-dd20-11ed-afa1-0242ac120002', 'Do или does', '... your sister speak French fluently?
+How often ... your brother go to the gym?
+... the store close at 9 PM today?
+Why ... your friend always forget to call you back?
+... your parents like to travel?
+When ... the next train to arrive?
+... your boss usually assign you a lot of work?
+What time ... the concert start tonight?
+... you prefer coffee or tea in the morning?
+... the company provide health insurance for its employees', 'does
+do
+does
+does
+do
+does
+does
+does
+do
+does', '258ed763-928c-4155-bee9-fdbaaadc15f3');
+INSERT INTO public.task (id, name, test, answers, sub_chapter_id) VALUES ('758ed763-928c-4155-bee9-fdbaaadc15f3', 'Am, is или are', 'He ... 22 years old
+There ... many people at the concert last night
+She ... tired after a long day at work.
+We ... planning a trip to Europe next summer.
+My sister ... studying for her exams all week.
+I ... smart.
+', 'is
+are
+is
+are
+is
+am', '258ed763-928c-4155-bee9-fdbaaadc15f3');
+
