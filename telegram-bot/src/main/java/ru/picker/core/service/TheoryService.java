005cb0ac815b803ce3b67ab47856169ae608b03b
Asteises <p.nikitaev@gmail.com>
2023-07-05 18:33:53 +0300

    add findById method to TheoryService

--- telegram-bot/src/main/java/ru/picker/core/service/TheoryService.java
+++ telegram-bot/src/main/java/ru/picker/core/service/TheoryService.java
@@ -2,11 +2,20 @@ package ru.picker.core.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.picker.core.entity.Theory;
 import ru.picker.core.repository.TheoryRepository;
+
+import javax.ws.rs.NotFoundException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TheoryService {
 
     private final TheoryRepository theoryRepository;
+
+    public Theory findById(UUID id) {
+        return theoryRepository.findById(id).orElseThrow(() ->
+                new NotFoundException(String.format("Theory with ID: %s not found", id)));
+    }
 }
